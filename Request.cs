4ff3f188ca8eab2e527c609eddb05c55ca2c79f7
Vishal Khanapure <vishal.khanapure@gmail.com>
2022-10-21 17:24:18 -0400

    Remove ProtoBuf and other dependencies that are no longer required and refactor code

---
+++ Request.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace TsmXiL
+{
+    public class Request
+    {
+        public double Acceleration { get; set; }
+
+        public byte[] GetByteArray()
+        {
+            using (var m = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(m))
+                {
+                    writer.Write(Acceleration);
+                }
+
+                return m.ToArray();
+            }
+        }
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TsmXiL
+{
+    public class Response
+    {
+        public double Acceleration { get; set; }
+        public double Speed { get; set; }
+
+        public static Response GetResponseFromByteArray(byte[] bytes)
+        {
+            var bf = new BinaryFormatter();
+            using (var m = new MemoryStream(bytes))
+            {
+                var obj = bf.Deserialize(m);
+                return (Response) obj;
+            }
+        }
+    }
+}
